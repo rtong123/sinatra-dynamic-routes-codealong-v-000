762dd37cfdf6e982c96d99f7cb71d5c25a236906
Rebecca Tong <rtong@buffalo.edu>
2018-06-29 21:33:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   # Code your final two routes here:
 
   get '/goodbye/:name' do
-    @jerome = params[:jerome]
+    @name = params[:name]
     "Goodbye #{@jerome}."
   end
 
